d2f18da0097201e049861e2d95766206f93b2592
Francisco Muñoz <femunoz@dim.uchile.cl>
2022-10-10 00:39:55 -0300

    refactor: Se mejoró la visualización de las recetas

--- recipes/models.py
+++ recipes/models.py
@@ -9,10 +9,10 @@ class Recipe(models.Model):
     Recipe class model for the Users' recipes.
     """
     # Llave foránea del usuario que escribe la receta
-    user_id = models.ForeignKey(
+    user: User = models.ForeignKey(
         User,
         on_delete=models.SET_NULL,
-        null=True
+        null=True,
     )
     # Nombre de la receta
     name = models.CharField(max_length=255)
@@ -21,11 +21,15 @@ class Recipe(models.Model):
     # Instrucciones de la preparación
     instructions = models.TextField()
     # Path de la imagen linkeada
-    image_path = models.CharField(max_length=255)
+    image_path = models.CharField(
+        max_length=255,
+        null=True,
+        blank=True,
+    )
 
     def __str__(self):
         return (f"Recipe("
                 f"id={self.id}, "
-                f"user_id={self.user_id}, "
+                f"user={self.user.get_username()}, "
                 f"name={self.name}"
                 f")")
